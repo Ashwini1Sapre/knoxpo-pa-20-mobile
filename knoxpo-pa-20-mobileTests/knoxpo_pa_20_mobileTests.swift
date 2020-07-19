cd4e8df65603afd51637259aa69b26040a86eb97
sysfort <sysfortiostesting@gmail.com>
2020-07-19 09:25:41 +0530

    Initial Commit

---
+++ knoxpo-pa-20-mobileTests/knoxpo_pa_20_mobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  knoxpo_pa_20_mobileTests.swift
+//  knoxpo-pa-20-mobileTests
+//
+//  Created by Girish Purandare on 19/07/20.
+//  Copyright © 2020 Girish Purandare. All rights reserved.
+//
+
+import XCTest
+@testable import knoxpo_pa_20_mobile
+
+class knoxpo_pa_20_mobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
